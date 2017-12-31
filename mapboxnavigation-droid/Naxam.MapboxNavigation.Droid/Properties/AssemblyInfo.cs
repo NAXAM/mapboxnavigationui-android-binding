22754be301082e08101a57f22247120fec7a580f
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-31 15:12:41 +0700

    Mapbox Navigation 0.8.0

--- mapboxnavigation-droid/Naxam.MapboxNavigation.Droid/Properties/AssemblyInfo.cs
+++ mapboxnavigation-droid/Naxam.MapboxNavigation.Droid/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyVersion("0.8.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
